a65eda730e3480b007eb51bbbbce0285dd1c2fee
niklas <niklasi@mail.uni-paderborn.de>
2022-06-02 17:08:48 -0400

    add node and npm

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#install npm and node for Jiff
+sudo apt update
+sudo apt install nodejs npm
+
+nodejs --version
+
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+
+
+sudo apt install nodejs
+
+
+node --version
+
+
