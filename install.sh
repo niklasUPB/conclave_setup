6ff84945f24eacdb737b4c34d18982ee252059dc
niklas <niklasi@mail.uni-paderborn.de>
2022-06-02 20:13:47 -0400

    cmake

--- install.sh
+++ install.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
 #install npm and node for Jiff
-sudo apt update
-sudo apt install nodejs npm
-
+if[$1 = jiff_d || $1 = all]
+then
+	sudo apt update
+	sudo apt install nodejs npm
+fi
 
 node --version
 nodejs --version
 
-cd ..
 
-sudo git clone https://github.com/multiparty/jiff.git
+if [$1 = jiff || $1 = all]
+then
+	cd ..
 
-cd jiff
+	sudo git clone https://github.com/multiparty/jiff.git
 
-sudo npm install
+	cd jiff
+
+	sudo npm install
+	cd ..
+	cd conclave_setup
+fi
+
+if[$1 = conda|| $1 = all]
+then
+	sudo apt install wget
+	bash Anaconda-latest-Linux-x86_64.sh
+
+fi
+
+
+
+
+
