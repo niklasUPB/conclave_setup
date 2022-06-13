60784b83d2c6e61c433af8ccf91c22fe5c924194
niklas <niklas.isermann@outlook.com>
2022-06-13 08:35:03 -0400

    timeout_fix

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
-var JIFFServer = require('/home/conclave_setup/jiff/lib/jiff-server');
-var jiff_instance = new JIFFServer(http, { logs: true });
+var JIFFServer = require('/home/kali/Desktop/jiff/lib/jiff-server');
+var jiff_instance = new JIFFServer(http, {
+  logs: true,
+  socketOptions: {
+    pingTimeout: 1000000,
+    pingInterval: 2000
+  }
+});
 
-var jiffBigNumberServer = require('/home/conclave_setup/jiff/lib/ext/jiff-server-bignumber');
+var jiffBigNumberServer = require('/home/kali/Desktop/jiff/lib/ext/jiff-server-bignumber');
 jiff_instance.apply_extension(jiffBigNumberServer);
 
-app.use("/home/conclave_setup/jiff/demos", express.static("demos"));
-app.use("/home/conclave_setup/jiff/lib", express.static("lib"));
-app.use("/home/conclave_setup/jiff/lib/ext", express.static("lib/ext"));
-app.use("/home/conclave_setup/jiff/bignumber.js", express.static("node_modules/bignumber.js"));
+app.use("/home/kali/Desktop/jiff/demos", express.static("demos"));
+app.use("/home/kali/Desktop/jiff/lib", express.static("lib"));
+app.use("/home/kali/Desktop/jiff/lib/ext", express.static("lib/ext"));
+app.use("/home/kali/Desktop/jiff/bignumber.js", express.static("node_modules/bignumber.js"));
 
 http.listen(9000, function()
 {
