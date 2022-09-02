e7b78ce6c0a0270de2d138077107db84211b42a3
niklas <105489239+niklasUPB@users.noreply.github.com>
2022-09-02 12:41:15 +0200

    Update protocol.py

--- protocol.py
+++ protocol.py
@@ -34,8 +34,8 @@ def protocol():
 	join1 = join(in1, in2, 'join1', ['primary_key'], ['primary_key'])
 	
 	collect(join1, 3)
-	collect(in3,3)
-	return {in1, in2,in3}
+	
+	return {in1, in2}
 
 
 if __name__ == "__main__":
