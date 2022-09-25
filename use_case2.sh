99ae36670beaa3ef123a367befdcef73d6edce36
niklas <niklas.isermann@outlook.com>
2022-09-25 16:31:49 -0400

    use_case2

---
+++ use_case2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+export NODE_PATH=/home/conclave_setup/jiff/node_modules
+echo $NODE_PATH
+export PYTHONPATH=$PYTHONPATH:/home/conclave_setup/conclave
+export NODE_OPTIONS=--max_old_space_size=32000
+sysctl -w vm.max_map_count=65530000
+
+
+
+if [ $1 = "server" ]
+then
+	if [ -d "./data" ]
+	then
+		rm -r data
+		mkdir data	
+	fi
+	if [ -d "./data2" ]
+	then
+		rm -r data2
+		mkdir data2
+	fi
+		
+
+
+	python3 create_input_use_case2.py $2 
+
+
+fi
+
+
+if [ $1 = "server" ]
+then
+	time --output=use_case2_local -a -f='(%Mmax)' node server.js
+fi
+if [ $1 = "eins" ]
+then
+	time --output=use_case2_local -a -f='(%Mmax)' python protocol.py config_1_use_case1.json
+fi	
+if [ $1 = "zwei" ]
+then
+	time --output=use_case2_local -a -v python protocol.py config_2_use_case1.json
+fi	
