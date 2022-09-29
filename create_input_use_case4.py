ce9564d3a5e6079aff74c6f151653ac411ca6751
niklas <niklas.isermann@outlook.com>
2022-09-29 00:24:42 -0400

    use_case

---
+++ create_input_use_case4.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import sys
+import csv
+import random
+
+
+def write_half(start,amount, writer):
+    for x in range(start, start+amount):
+        line = []
+        line.append( str(x) )
+        line.append(str( random.randint(0, 1000 )))
+        writer.writerow(line)
+def write_file(name, row, intersection , start ):
+    file = open(name , "w")
+    writer = csv.writer(file)
+
+    if name == "data/left.csv":
+       writer.writerow([ "a", "b"])
+    if name == "data2/left2.csv":
+       writer.writerow([ "a", "b"])
+    if name == "data/right.csv":
+        writer.writerow(["c", "d"])
+    if name == "data/right2.csv":
+        writer.writerow(["c", "d"])
+    write_half(start,intersection, writer)
+    write_half(row * 10, row-intersection , writer)
+    file.close()
+
+
+
+if __name__ == "__main__":
+    rows =  int (sys.argv[1]);
+    intersection_size = int((int(sys.argv[2])*0.5));
+    half = int(int(rows) * 0.5)
+    write_file("data/right.csv",  half, int(intersection_size), 0)
+    write_file("data2/right2.csv", half, int(intersection_size),rows )
+    write_file("data/left.csv", half, int(intersection_size), 0)
+    write_file("data2/left2.csv", half, int(intersection_size),rows)
+    file = open("Results/use_case4_local" , "a")
+    writer = csv.writer(file)
+    line = []
+    line.append(rows)
+    line.append(rows)
+    writer.writerow(line)
+    file.close()
+
+
+
---
+++ use_case4.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+export NODE_PATH=/home/conclave_setup/jiff/node_modules
+echo $NODE_PATH
+export PYTHONPATH=$PYTHONPATH:/home/conclave_setup/conclave
+export NODE_OPTIONS=--max_old_space_size=128000
+sysctl -w vm.max_map_count=65530000
+
+
+
+if [ $1 = "server" ]
+then
+	if [ -d "./data" ]
+	then
+		rm -r data
+		mkdir data	
+	fi
+	if [ -d "./data2" ]
+	then
+		rm -r data2
+		mkdir data2
+	fi
+		
+
+
+	python3 create_input_use_case4.py $2  $3 
+
+
+fi
+
+
+if [ $1 = "server" ]
+then
+	time --output=./Results/use_case4_local -a -v node server.js
+fi
+if [ $1 = "eins" ]
+then
+	time --output=./Results/use_case4_local -a -v python protocol_use_case3.py config_1_use_case4.json
+fi	
+if [ $1 = "zwei" ]
+then
+	time --output=./Results/use_case4_local -a -v python protocol_use_case3.py config_2_use_case4.json
+fi	
