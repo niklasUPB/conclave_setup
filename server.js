88143667ff62e33e02504d8a6ca91216a959de0c
niklas <105489239+niklasUPB@users.noreply.github.com>
2022-10-08 12:21:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var jiff_instance = new JIFFServer(http, {
 	// 2 mal 500000 ging für 40 mit reconnect.
 	//5000000 ging ganz gut 
   socketOptions: {
-    pingTimeout: 50000000,
-    pingInterval: 50000000
+    pingTimeout: 5000000,
+    pingInterval: 5000000
   }
 });
 
