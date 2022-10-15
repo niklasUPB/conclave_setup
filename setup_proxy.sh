08c5dec3a7f84fd4e2864d84e01cde7116cc1420
niklas <105489239+niklasUPB@users.noreply.github.com>
2022-10-15 23:57:47 +0200

    Update setup_proxy.sh

--- setup_proxy.sh
+++ setup_proxy.sh
@@ -2,6 +2,6 @@
 toxiproxy-cli create Conclave --listen 127.0.0.01:9000 --upstream 127.0.0.1:9005 
 toxiproxy-cli toxic add Conclave -t latency -n latency_upstream -a latency=100 --upstream
 toxiproxy-cli toxic add Conclave -t latency -n latency_downstream -a latency=100 --downstream
-toxiproxy-cli toxic add Conclave -t bandwidth -n bandwidth_upstream -a rate=5000 --upstream
-toxiproxy-cli toxic add Conclave -t bandwidth -n bandwidth_downstream -a rate=5000 --downstream
+toxiproxy-cli toxic add Conclave -t bandwidth -n bandwidth_upstream -a rate=10000 --upstream
+toxiproxy-cli toxic add Conclave -t bandwidth -n bandwidth_downstream -a rate=10000 --downstream
 
